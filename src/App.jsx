f0a827a3a2d80b87232bac1e616823e4ea86a178
Aasthakumari2025 <aasthakumari5555@gmail.com>
2025-10-03 14:05:39 +0530

    registration project

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Layout from './component/Layout';
 import Home from './component/Home';
 import Registration from './component/Registration';
@@ -7,16 +7,14 @@ import Verify from './component/Verify';
 
 function App() {
   return (
-    <HashRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} /> 
-          <Route path="/register" element={<Registration />} />
-          <Route path="/Meetup" element={<MeetupRegist />} />
-          <Route path="/verify" element={<Verify />} />
-        </Routes>
-      </Layout>
-    </HashRouter>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} /> 
+        <Route path="/register" element={<Registration />} />
+        <Route path="/Meetup" element={<MeetupRegist />} />
+        <Route path="/verify" element={<Verify />} />
+      </Routes>
+    </Layout>
   );
 }
 
